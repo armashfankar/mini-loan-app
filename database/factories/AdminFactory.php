f9fcb9457c41b5f1631d9a0107eab901f98d37db
armashfankar <armashfankar@gmail.com>
2022-09-24 02:42:51 +0530

    Create Default Admin User Seeder

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -2,11 +2,19 @@
 
 namespace Database\Factories;
 
+use App\Models\Admin;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-class UserFactory extends Factory
+class AdminFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Admin::class;
+
     /**
      * Define the model's default state.
      *
@@ -15,11 +23,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'admin_reference_number' => Str::random(20),
+            'name' => "Thanh Nghiem",
+            'email' => "admin@aspireapp.com",
+            'password' => '$2y$10$KeHthg2WhwIgJWGtBdhtq.xF6L9Y0PaVh.kXfuJIw5xxW02aTawKS', // asdasdasd
         ];
     }
 
